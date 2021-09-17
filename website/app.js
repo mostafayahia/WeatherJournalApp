2cd714920a387a181bd897958455c041b3fc645f
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-09-17 19:51:00 +0200

    feat: get web api data for today

--- website/app.js
+++ website/app.js
@@ -1,12 +1,33 @@
 // Personal API Key for OpenWeatherMap API
+const API_KEY = 'INSERT YOUR API KEY HERE';
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather/';
 
 // Event listener to add function to existing HTML DOM element
+document.querySelector('#generate').addEventListener('click', generateBtnHandler);
+
 
 /* Function called by event listener */
+function generateBtnHandler(event) {
+    event.preventDefault();
+
+    const zip = document.querySelector('#zip').value;
+
+    getWebApiData(zip)
+        .then(function (data) {
+            console.dir(data)
+        })
+}
 
 /* Function to GET Web API Data*/
-
-/* Function to POST data */
+async function getWebApiData(zip, baseUrl = BASE_URL, apiKey = API_KEY) {
+    const response = await fetch(baseUrl + `?zip=${zip}&appid=${apiKey}`);
+    try {
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('error', error)
+    }
+}
 
 
 /* Function to GET Project Data */

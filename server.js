52cfece68b7df43793644aa5b30f995073db7b87
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-09-17 11:59:43 +0200

    feat: add basic skeleton for server.js

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 // Setup empty JS object to act as endpoint for all routes
+const data = [{ key: 'val', key2: 'val2' }];
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
 /* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Spin up the server
 // Callback to debug
+const port = 3000;
+app.listen(port, function () {
+    console.log(`Server is running on port: ${port}`)
+});
 
 // Initialize all route with a callback function
+app.get('/all', getAllData);
 
 // Callback function to complete GET '/all'
+function getAllData(req, res) {
+    res.send(data);
+}
 
 // Post Route
-  
